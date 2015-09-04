910336932e71b28eb05c259eabff4f9016a92293
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-09-04 13:06:45 +0200

    add basic unit tests

--- spec/outputs/mongodb_spec.rb
+++ spec/outputs/mongodb_spec.rb
@@ -16,4 +16,29 @@ describe LogStash::Outputs::Mongodb do
     plugin = LogStash::Plugin.lookup("output", "mongodb").new(config)
     expect {plugin.register}.to_not raise_error
   end
+
+  describe "#send" do
+
+    subject { LogStash::Outputs::Mongodb.new(config) }
+
+    let(:properties) { { "message" => "This is a message!"} }
+    let(:event)      { LogStash::Event.new(properties) }
+    let(:connection) { double("connection") }
+    let(:client)     { double("client") }
+    let(:collection) { double("collection") }
+
+    before(:each) do
+      allow(Mongo::Client).to receive(:new).and_return(connection)
+      allow(connection).to receive(:use).and_return(client)
+      allow(client).to receive(:[]).and_return(collection)
+      allow(collection).to receive(:insert_one)
+      subject.register
+    end
+
+    it "should send the event to the database" do
+      expect(collection).to receive(:insert_one)
+      subject.receive(event)
+    end
+  end
+
 end
