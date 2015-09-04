06a70e85117b7c7715c037220871fbdb001bb4c5
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-09-04 11:02:04 +0200

    versino 0.2.0 bump with basic registration test to the project

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 0.2.0
+  - Add basic registration test to the project
--- spec/outputs/mongodb_spec.rb
+++ spec/outputs/mongodb_spec.rb
@@ -1,1 +1,20 @@
+# encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
+require "logstash/plugin"
+require "logstash/outputs/mongodb"
+
+describe LogStash::Outputs::Mongodb do
+
+  let(:uri)        { 'mongodb://localhost:27017' }
+  let(:database)   { 'logstash' }
+  let(:collection) { 'logs' }
+
+  let(:config) do
+    { "uri" => uri, "database" => database, "collection" => collection }
+  end
+
+  it "should register" do
+    plugin = LogStash::Plugin.lookup("output", "mongodb").new(config)
+    expect {plugin.register}.to_not raise_error
+  end
+end
