692f364e793d82898ddff2b37b2649afc311b8df
guyboertje <guyboertje@gmail.com>
2015-12-09 17:56:06 +0000

    make tests more robust by using to_bson from BSON::Float and BSON::Time mixins

--- spec/bson/big_decimal_spec.rb
+++ spec/bson/big_decimal_spec.rb
@@ -5,7 +5,7 @@ require 'stringio'
 
 describe ::BigDecimal do
   let(:a_number) { "4321.1234" }
-  let(:bson_number) { "Tt$\x97\x1F\xE1\xB0@".force_encoding(BSON::BINARY) }
+  let(:bson_number) { 4321.1234.to_bson }
 
   subject { described_class.new(a_number) }
 
@@ -14,18 +14,17 @@ describe ::BigDecimal do
   end
 
   it "to_bson returns a binary encoded number" do
-    expect(subject.to_bson).to eq(bson_number)
+    expect(subject.to_bson).to eq(4321.1234.to_bson)
   end
 
   it "bson_type returns a binary encoded 1" do
-    expect(subject.bson_type).to eq("\x01".force_encoding(BSON::BINARY))
+    expect(subject.bson_type).to eq(12.34.bson_type)
   end
 
   describe "class methods" do
     it "builds a new BigDecimal from BSON" do
-      decoded = described_class.from_bson(StringIO.new(bson_number))
-      expect(decoded).to eq(subject)
-      expect(decoded).to be_a(BigDecimal)
+      decoded = described_class.from_bson(StringIO.new(4321.1234.to_bson))
+      expect(decoded).to eql(BigDecimal.new(a_number))
     end
   end
 end
--- spec/bson/logstash_timestamp_spec.rb
+++ spec/bson/logstash_timestamp_spec.rb
@@ -3,28 +3,29 @@ require_relative "../spec_helper"
 require 'stringio'
 
 describe ::LogStash::Timestamp do
-  let(:a_time) { Time.utc(1918,11,11,11,0,0) }
-  let(:bson_time) { "\x80{y@\x88\xFE\xFF\xFF".force_encoding(BSON::BINARY) }
+  let(:time_array) { [1918,11,11,11,0,0, "+00:00"] }
+  let(:a_time) { Time.utc(*time_array) }
+  let(:bson_time) { Time.utc(*time_array).to_bson }
 
-  subject { described_class.new(a_time) }
+  subject(:timestamp) { described_class.new(a_time) }
 
   it "responds to to_bson" do
     expect(subject).to respond_to(:to_bson)
   end
 
   it "to_bson returns a binary encoded timestamp" do
-    expect(subject.to_bson).to eq(bson_time)
+    expect(timestamp.to_bson).to eq(bson_time)
   end
 
   it "bson_type returns a binary encoded 9" do
-    expect(subject.bson_type).to eq("\x09".force_encoding(BSON::BINARY))
+    expect(subject.bson_type).to eq(a_time.bson_type)
   end
 
   describe "class methods" do
     it "builds a new Timestamp from BSON" do
-      decoded = described_class.from_bson(StringIO.new(bson_time))
-      expect(decoded).to eq(subject)
-      expect(decoded).to be_a(::LogStash::Timestamp)
+      expected = ::LogStash::Timestamp.new(a_time)
+      decoded = ::LogStash::Timestamp.from_bson(StringIO.new(bson_time))
+      expect(decoded <=> expected).to eq(0)
     end
   end
 end
